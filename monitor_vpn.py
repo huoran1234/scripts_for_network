0a2b54c8808fb2edca6eab845b3720eb8be1b58d
huoran1234 <67900426+huoran1234@users.noreply.github.com>
2021-12-22 15:46:06 +0800

    Update monitor_vpn.py

--- monitor_vpn.py
+++ monitor_vpn.py
@@ -20,40 +20,16 @@ BJ_C2811 = {"host": "192.168.63.6",
 Connect = ConnectHandler(**BJ_C2811)
 Connect.enable()
 
-ping_cd = "ping 192.168.255.2 repeat 10"
-ping_sz = "ping 192.168.255.3 repeat 10"
-ping_cd_result = Connect.send_command(ping_cd)
-ping_sz_result = Connect.send_command(ping_sz)
-ping_cd_filter = re.findall('!',ping_cd_result)
-ping_sz_filter = re.findall('!',ping_sz_result)
-ping_cd_counter = len(ping_cd_filter)
-ping_sz_counter = len(ping_sz_filter)
-
-if ping_cd_counter < 9:
-        Connect.send_command("clear vpdn tunnel pptp ip remote 10.113.29.160")
-        logtofile(ping_cd_result,"CD Site","disconnected")
-else:
-        logtofile(ping_cd_result,"CD Site","connected")
-
-if ping_sz_counter < 9:
-        Connect.send_command("clear vpdn tunnel pptp ip remote 10.5.26.245")
-        logtofile(ping_sz_result,"SZ Site","disconnected") 
-else:
-        logtofile(ping_sz_result,"SZ Site","connected")
+ping_ip = ("192.168.255.2","192.168.255.3")
+vpdn_ip = ("10.113.29.160","10.5.26.245")
+site_name = ("CD","SZ")
+for i in range (2):
+    ping_result = Connect.send_command("ping " + ping_ip[i] + " repeat 10")
+    ping_count = len(re.findall("!",ping_result))
+    if ping_count < 9:
+        Connect.send_command("clear vpdn tunnel pptp ip remote " + vpdn_ip[i])
+        logtofile(ping_result,site_name[i] + " Site","disconnected")
+    else:
+        logtofile(ping_result,site_name[i] + " Site","connected")
 
 Connect.disconnect()
-
-#For Debugging
-#print(ping_cd_result)
-#print(ping_cd_filter)
-#print(ping_cd_counter)
-#print(ping_sz_result)
-#print(ping_sz_filter)
-#print(ping_sz_counter)
-
-#Sample for netmiko
-#Connect.enable()
-#To_Issue = "show ip interface brief"
-#print(Connect.send_command(To_Issue))
-#clear_vpdn_command = "clear vpdn tunnel pptp all"
-#output = Connect.send_command(clear_vpdn_command, expect_string="[confirm]")
